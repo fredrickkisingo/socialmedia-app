0aa6c451bebc33d3d64b072cd9f3e68b85072c3c
fred <fredrickkisingo@gmail.com>
2020-07-21 19:50:09 +0300

    using jquery to fetch posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -29,9 +29,11 @@
                 </div>
                 <div class="interaction">
                    <a href="#"> <i class="fa fa-heart"  aria-hidden="true"></i></a>
-                   <a href=""> <i class="fa fa-thumbs-down"></i></a>
+                   <a href="#"> <i class="fa fa-thumbs-down"></i></a>
+
+
                    @if(Auth::user() == $post->user)
-                   <a href="#">Edit</a>
+                   <a href="#" class="edit">Edit</a></a>
                    <a href="{{route('post.delete',['post_id'=>$post->id])}}">Delete</a>
                    @endif   
                 </div>
@@ -39,5 +41,29 @@
             </article>
             @endforeach
         </div>
-    </section>    
+    </section> 
+    <div class="modal" tabindex="-1" role="dialog" id="edit-modal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Edit  Post</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                      <label for="post-body">Edit the Post here</label>
+                      <textarea class="form-control" name="post-body" id="post-body" rows="5"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div   
  @endsection
